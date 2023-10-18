3f0e6189b7ae4ade71e154a35a407a460f18bc81
Ashley Needham <ashley@needham.online>
2023-10-18 23:16:09 +0100

    Reduce font size on answers

--- js/plant-list.js
+++ js/plant-list.js
@@ -7,30 +7,35 @@ let plant_list = [
                 "credit_owner": "",
                 "credit_date": "",
                 "credit_url": "",
+                "licence_url": "",
                 "filename": "Taraxacum_officinale_1.jpg"
             },
             {
                 "credit_owner": "",
                 "credit_date": "",
                 "credit_url": "",
+                "licence_url": "",
                 "filename": "Taraxacum_officinale_2.jpg"
             },
             {
                 "credit_owner": "",
                 "credit_date": "",
                 "credit_url": "",
+                "licence_url": "",
                 "filename": "Taraxacum_officinale_3.jpg"
             },
             {
                 "credit_owner": "",
                 "credit_date": "",
                 "credit_url": "",
+                "licence_url": "",
                 "filename": "Taraxacum_officinale_4.jpg"
             },
             {
                 "credit_owner": "",
                 "credit_date": "",
                 "credit_url": "",
+                "licence_url": "",
                 "filename": "Taraxacum_officinale_5.jpg"
             }
         ]
@@ -43,36 +48,42 @@ let plant_list = [
                 "credit_owner": "",
                 "credit_date": "",
                 "credit_url": "",
+                "licence_url": "",
                 "filename": "Ranunculus_repens_1.jpg"
             },
             {
                 "credit_owner": "",
                 "credit_date": "",
                 "credit_url": "",
+                "licence_url": "",
                 "filename": "Ranunculus_repens_2.jpg"
             },
             {
                 "credit_owner": "",
                 "credit_date": "",
                 "credit_url": "",
+                "licence_url": "",
                 "filename": "Ranunculus_repens_3.jpg"
             },
             {
                 "credit_owner": "",
                 "credit_date": "",
                 "credit_url": "",
+                "licence_url": "",
                 "filename": "Ranunculus_repens_4.jpg"
             },
             {
                 "credit_owner": "Gemma Needham",
                 "credit_date": "2023-10-15",
                 "credit_url": "",
+                "licence_url": "",
                 "filename": "Ranunculus_repens_5.jpg"
             },
             {
                 "credit_owner": "Gemma Needham",
                 "credit_date": "2024-10-15",
                 "credit_url": "",
+                "licence_url": "",
                 "filename": "Ranunculus_repens_6.jpg"
             }
         ]
@@ -85,24 +96,28 @@ let plant_list = [
                 "credit_owner": "",
                 "credit_date": "",
                 "credit_url": "",
+                "licence_url": "",
                 "filename": "Buddleja_davidii_1.jpg"
             },
             {
                 "credit_owner": "",
                 "credit_date": "",
                 "credit_url": "",
+                "licence_url": "",
                 "filename": "Buddleja_davidii_2.jpg"
             },
             {
                 "credit_owner": "",
                 "credit_date": "",
                 "credit_url": "",
+                "licence_url": "",
                 "filename": "Buddleja_davidii_3.jpg"
             },
             {
                 "credit_owner": "Gemma Needham",
                 "credit_date": "2023-10-15",
                 "credit_url": "",
+                "licence_url": "",
                 "filename": "Buddleja_davidii_4.jpg"
             }
         ]
@@ -115,25 +130,63 @@ let plant_list = [
                 "credit_owner": "",
                 "credit_date": "",
                 "credit_url": "",
+                "licence_url": "",
                 "filename": "Urtica_dioica_1.jpg"
             },
             {
                 "credit_owner": "",
                 "credit_date": "",
                 "credit_url": "",
+                "licence_url": "",
                 "filename": "Urtica_dioica_1.jpg"
             },
             {
                 "credit_owner": "",
                 "credit_date": "",
                 "credit_url": "",
+                "licence_url": "",
                 "filename": "Urtica_dioica_1.jpg"
             },
             {
                 "credit_owner": "",
                 "credit_date": "",
                 "credit_url": "",
+                "licence_url": "",
                 "filename": "Urtica_dioica_1.jpg"
+            }
+        ]
+    },
+    {
+        "common_name": "Virginia creeper",
+        "botanical_name": "Parthenocissus quinquefolia",
+        "pictures": [
+            {
+                "credit_owner": "Sesamehoneytart, CC BY-SA 3.0, via Wikimedia Commons",
+                "credit_date": "2013-07-17",
+                "credit_url": "https://commons.wikimedia.org/wiki/File:Parthenocissus_quinquefolia_virginia_creeper.JPG",
+                "licence_url": "https://creativecommons.org/licenses/by-sa/3.0/",
+                "filename": "Parthenocissus_quinquefolia1.jpg"
+            },
+            {
+                "credit_owner": "Jay Sturner from USA, CC BY 2.0, via Wikimedia Commons",
+                "credit_date": "2003-05-21",
+                "credit_url": "https://commons.wikimedia.org/wiki/File:Virginia_Creeper_(Parthenocissus_quinquefolia)_-_Flickr_-_Jay_Sturner.jpg",
+                "licence_url": "https://creativecommons.org/licenses/by-sa/2.0/",
+                "filename": "Parthenocissus_quinquefolia_2.jpg"
+            },
+            {
+                "credit_owner": "Franzgorg, CC BY-SA 4.0, via Wikimedia Commons",
+                "credit_date": "2017-06-21",
+                "credit_url": "https://commons.wikimedia.org/wiki/File:Parthenocissus_quinquefolia_leaf.jpg",
+                "licence_url": "https://creativecommons.org/licenses/by-sa/4.0/",
+                "filename": "Parthenocissus_quinquefolia_3.jpg"
+            },
+            {
+                "credit_owner": "Photo by David J. Stang, CC BY-SA 4.0, via Wikimedia Commons",
+                "credit_date": "2006-08-16",
+                "credit_url": "https://commons.wikimedia.org/wiki/File:Parthenocissus_quinquefolia_3zz.jpg",
+                "licence_url": "https://creativecommons.org/licenses/by-sa/4.0/",
+                "filename": "Parthenocissus_quinquefolia_4.jpg"
             }
         ]
     },
